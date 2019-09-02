e08f3d274cb53a34519e9602178eefd50f9f3863
CPoku <Charlespokuaa1@gmail.com>
2019-09-02 12:47:10 +0200

    I will commit

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -3,7 +3,7 @@ package h01;
 /**
  * Created by piet on 31-08-16.
  */
-public class HalloRocDev {
+public class Idk verandering van text {
 
     public static void main(String[] args) {
         System.out.println("Hallo klas 8A!");
