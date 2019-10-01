94dd90b7d15947b3e6409fa272ab4e401feff067
CPoku <Charlespokuaa1@gmail.com>
2019-10-01 14:21:48 +0200

    Tot opdracht 8.2

--- src/h08/Opdracht2.java
+++ src/h08/Opdracht2.java
@@ -10,33 +10,48 @@ public class Opdracht2 extends Applet {
     Button knop2;
     Button knop3;
     Button knop4;
-    int counterMannen;
-    int CounterVrouwen;
-    int CounterJongens;
-    int CounterMeisjes;
     int Totaal;
-    int a = +1;
-    TextField tekstvak;
+    int getal1;
+    int getal2;
+    int getal3;
+    int getal4;
+
+
 
     public void init() {
-        tekstvak = new TextField();
 
         knop1 = new Button("Mannen");
+        knop1.addActionListener( new Knop1Listener() );
         add(knop1);
 
+
         knop2 = new Button("Vrouwen");
+        knop2.addActionListener( new Knop2Listener() );
         add(knop2);
 
         knop3 = new Button("Jongens");
+        knop3.addActionListener( new Knop3Listener() );
         add(knop3);
 
         knop4 = new Button("Meisjes");
+        knop4.addActionListener( new Knop4Listener() );
         Component add = add( knop4 );
+
+
+
+        getal1 = 0;
+        getal2 = 0;
+        getal3 = 0;
+        getal4 = 0;
 
     }
 
     public void paint(Graphics g) {
-      g.drawString(" Mannen " + a ,50,70);
+      g.drawString(  " Mannen "  + ( getal1 ),50,70 );
+        g.drawString(" Vrouwen " + ( getal2 ),50,90 );
+        g.drawString(" Jongens " + ( getal3 ),50,110 );
+        g.drawString(" Meisjes " + ( getal4 ),50,130 );
+        g.drawString(" Totaal  " + ( Totaal ),50 ,170 );
       
 
     }
@@ -44,6 +59,8 @@ public class Opdracht2 extends Applet {
     class Knop1Listener implements ActionListener {
 
         public void actionPerformed(ActionEvent e) {
+            getal1 ++;
+            Totaal ++;
             repaint();
             
         }
@@ -52,7 +69,8 @@ public class Opdracht2 extends Applet {
     class Knop2Listener implements ActionListener {
 
         public void actionPerformed(ActionEvent e) {
-            String s = tekstvak.getText();
+            getal2 ++;
+            Totaal++;
             repaint();
         }
 
@@ -63,7 +81,8 @@ public class Opdracht2 extends Applet {
     class Knop3Listener implements ActionListener {
 
         public void actionPerformed(ActionEvent e) {
-
+            getal3 ++;
+            Totaal ++;
             repaint();
         }
     }
@@ -71,7 +90,8 @@ public class Opdracht2 extends Applet {
     class Knop4Listener implements ActionListener {
 
         public void actionPerformed(ActionEvent e) {
-
+            getal4 ++;
+            Totaal ++;
             repaint();
 
         }
